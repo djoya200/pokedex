d2b7ad774cea11b07cc7316c3452d9b02a62a458
Desiree Joya <desireejoya@gmail.com>
2021-04-24 22:50:52 -0400

    added styling to page and background color changes based on type of pokemon

--- app.js
+++ app.js
@@ -1,16 +1,5 @@
-//link for api for pokemon
-//onlcik ufnction to acutally search for info
-//be able to input users search into the api
-//populate the data
-// fetch the data
-//async await to grab data
-// prevent default into form population
-
-// runs the whole thing
-
-console.log('app.js connected')
-//document.ready function
 $(() => {
+
     $('form').on("submit", (event) => {
         event.preventDefault()
         // seat a var for the user input
@@ -22,13 +11,64 @@ $(() => {
             //change the data to json data
             const data = await response.json()
             console.log(data);
+            //    $('.pokeImg').html(data.other.official-artwork)
             $('#pokeName').html(data.forms[0].name)
-            console.log("data.types[0].type[0]")
-            console.log(data.types[0].type.name)
             $('#pokeType').html(data.types[0].type.name)
+            $('#pokeNum').html(data.id)
+            // console.log($('#pokeType').html(data.types[0].type.name))
+            // console.log("image for pokemon")
+            // console.log(data.sprites.other.front_default)
+            // console.log(data.sprites.other["official-artwork"].front_default)
+            document.getElementById("pokeImg").src = data.sprites.other["official-artwork"].front_default
+
+// ----------------changes background color for each type of pokemon-------------------
+            if (data.types[0].type.name == "bug") {
+                $(".backOfThePage").css("background-color", "#94bc4a")
+            } else if(data.types[0].type.name == "dark") {
+                $(".backOfThePage").css("background-color", "#736c75")
+            } else if(data.types[0].type.name == "dragon") {
+                $(".backOfThePage").css("background-color", "#6a7baf")
+            } else if(data.types[0].type.name == "electric") {
+                $(".backOfThePage").css("background-color", "#e5c351")
+            } else if(data.types[0].type.name == "fairy") {
+                $(".backOfThePage").css("background-color", "#e397d1")
+            } else if(data.types[0].type.name == "fighting") {
+                $(".backOfThePage").css("background-color", "#cb5f48")
+            } else if(data.types[0].type.name == "fire") {
+                $(".backOfThePage").css("background-color", "#ea7a3c")
+            } else if(data.types[0].type.name == "flying") {
+                $(".backOfThePage").css("background-color", "#7da6de")
+            } else if(data.types[0].type.name == "ghost") {
+                $(".backOfThePage").css("background-color", "#846ab6")
+            } else if(data.types[0].type.name == "grass") {
+                $(".backOfThePage").css("background-color", "#71c558")
+            } else if(data.types[0].type.name == "ground") {
+                $(".backOfThePage").css("background-color", "#cc9f4f")
+            } else if(data.types[0].type.name == "ice") {
+                $(".backOfThePage").css("background-color", "#70cbd4")
+            } else if(data.types[0].type.name == "normal") {
+                $(".backOfThePage").css("background-color", "#aab09f")
+            } else if(data.types[0].type.name == "poison") {
+                $(".backOfThePage").css("background-color", "#b468b7")
+            } else if(data.types[0].type.name == "psychic") {
+                $(".backOfThePage").css("background-color", "#e5709b")
+            } else if(data.types[0].type.name == "rock") {
+                $(".backOfThePage").css("background-color", "#b2a061")
+            } else if(data.types[0].type.name == "steel") {
+                $(".backOfThePage").css("background-color", "#89a1b0")
+            } else if(data.types[0].type.name == "water") {
+                $(".backOfThePage").css("background-color", "#539ae2")
+            }
+
         }
 
         getPokeData()
     })
 })
 
+// $(() => {
+//         if($('#pokeType').html(data.types[0].type.name) == "fairy"){
+//             $(".backOfThePage").css("background-color:" , "#e397d1")
+//         }
+
+//     })
